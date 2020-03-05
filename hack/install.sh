aec692d3b51ed8824c5685764d453d7acf1e81a8
Zheng SHAO <axot@msn.com>
2020-03-05 19:02:19 +0900

    update zim

--- hack/install.sh
+++ hack/install.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env zsh
 
 echo install zim first
-git clone --recursive https://github.com/zimfw/zimfw.git ${ZDOTDIR:-${HOME}}/.zim
-for template_file in ${ZDOTDIR:-${HOME}}/.zim/templates/*; do
-  user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
-  cat ${template_file} ${user_file}(.N) > ${user_file}.tmp && mv ${user_file}{.tmp,}
-done
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 source ${ZDOTDIR:-${HOME}}/.zlogin
 
 echo install zplug
