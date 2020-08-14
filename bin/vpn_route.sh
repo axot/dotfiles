f57b8f900a505651210d724df9ed57dea140ed9a
Zheng SHAO <axot@msn.com>
2020-08-14 09:42:31 +0900

    add vpn route

---
+++ bin/vpn_route.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# show network service list
+# networksetup -listallnetworkservices
+gw=$(networksetup -getinfo Wi-Fi | grep '^Router' | awk '{print $2}')
+cmd=$1
+
+# fetch all google cidr
+google_cidr=$(for domain in `dig @8.8.8.8 _spf.google.com TXT +short | sed -e 's/"v=spf1//' -e 's/ ~all"//' -e 's/ include:/ /g' | xargs`; do
+dig @8.8.8.8 $domain TXT +short | sed -e 's/"v=spf1//' -e 's/ ~all"//' -e 's/ include:/ /g' -e 's/ip.://g' | xargs | grep -v ::
+done | tr '\n' ' ')
+
+# add routing
+## googles
+for cidr in $google_cidr; do
+route add -net $cidr $gw || true
+done
+
+if [ "$cmd" == "rm" ]; then
+    for cidr in $google_cidr; do
+    route delete -net $cidr $gw || true
+    done
+fi
+
+# show current route table
+netstat -nr -f inet
+
+# test route table
+route get 74.125.250.1
