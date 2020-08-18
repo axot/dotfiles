5aa484998530f2370f48c4001a8dad59a0d63687
Zheng SHAO <axot@msn.com>
2020-08-18 20:55:05 +0900

    Revert "add aws cidr for vpn bypass"
    This reverts commit c0de2751591c1f93a5ba4303ee9638358ea270a7.

--- bin/vpn_route.sh
+++ bin/vpn_route.sh
@@ -10,17 +10,9 @@ google_cidr=$(for domain in `dig @8.8.8.8 _spf.google.com TXT +short | sed -e 's
 dig @8.8.8.8 $domain TXT +short | sed -e 's/"v=spf1//' -e 's/ ~all"//' -e 's/ include:/ /g' -e 's/ip.://g' | xargs | grep -v ::
 done | tr '\n' ' ')
 
-# also for aws
-aws_cidr=$(curl -Ss -o - https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes[].ip_prefix' | tr '\n' ' ')
-
 # add routing
 ## googles
 for cidr in $google_cidr; do
-route add -net $cidr $gw || true
-done
-
-## aws
-for cidr in $aws_cidr; do
 route add -net $cidr $gw || true
 done
 
