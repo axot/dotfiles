8b98f91988f8f272e66c9657327381adee65396e
Zheng SHAO <axot@msn.com>
2022-02-12 23:13:18 +0900

    remove brew full path

--- bin/update_brew.sh
+++ bin/update_brew.sh
@@ -6,25 +6,25 @@ exec 2<&-
 exec 1<>"$LOG"
 
 echo Update homebrew
-/usr/local/bin/brew update 2>&1
+brew update 2>&1
 echo %
 
 echo Upgrade pkgs
-/usr/local/bin/brew upgrade 2>&1
+brew upgrade 2>&1
 echo %
 
 PASS=`security find-generic-password -a ${USER} -s brew_cu -w`
 sudo -S -v <<< $PASS
 
 echo Update Cask
-/usr/local/bin/brew cu -fay 2>&1
+brew cu -fay 2>&1
 echo %
 
 echo Clean up
-/usr/local/bin/brew cleanup -s >/dev/null 2>&1
-/usr/local/bin/brew cask cleanup >/dev/null 2>&1
-rm -rf $(/usr/local/bin/brew --cache)/*.tar.gz >/dev/null 2>&1
-rm -rf $(/usr/local/bin/brew --cache)/Cask/*.dmg >/dev/null 2>&1
+brew cleanup -s >/dev/null 2>&1
+brew cask cleanup >/dev/null 2>&1
+rm -rf $(brew --cache)/*.tar.gz >/dev/null 2>&1
+rm -rf $(brew --cache)/Cask/*.dmg >/dev/null 2>&1
 
 cat "$LOG" | mail -s 'RE: PKGs Update Report' axot@msn.com
 rm "$LOG"
