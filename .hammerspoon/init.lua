67a57b65c90599c62fe878b128409ea76a24fc30
Zheng Shao <axot@msn.com>
2023-09-20 15:20:13 +0900

    support mouse side buttons(not stable)

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,165 +1,195 @@
 local logger = hs.logger.new("MySpoon", "info")
 
-local function has_value (tab, val)
-    for index, value in ipairs(tab) do
+-- Helper Functions
+local function has_value(tab, val)
+    for _, value in ipairs(tab) do
         if value == val then
             return true
         end
     end
-
     return false
 end
 
-function pressFn(mods, key)
+local function logMapping(metaFrom, keyFrom, metaTo, keyTo)
+    logger.d("Mapping " ..
+        tostring(metaFrom) .. tostring(keyFrom) .. " -> " ..
+        tostring(metaTo) .. tostring(keyTo))
+end
+
+-- Key Mapping and Remapping Functions
+local function pressFn(mods, key)
     if key == nil then
         key = mods
         mods = {}
     end
-
     return function() hs.eventtap.keyStroke(mods, key, 1000) end
 end
 
-function appRemap(mods, key, remapMods, remapKey)
-    fn = pressFn(remapMods, remapKey)
-    return hs.hotkey.new(mods, key, fn, nil, fn)
+local function appRemap(metaFrom, keyFrom, metaTo, keyTo)
+    local fn = pressFn(metaTo, keyTo)
+    return hs.hotkey.new(metaFrom, keyFrom, fn, nil, fn)
 end
 
--- Translate user input keymap to hs.hotkey functions
-function keymapToHotkeys(keymap)
+local function keymapToHotkeys(keymap)
     local hotkeys = {}
-
-    for i, item in pairs(keymap) do
-        metaFrom = item[1]
-        keyFrom = item[2]
-        metaTo = item[3]
-        keyTo = item[4]
-
+    for _, item in pairs(keymap) do
+        local metaFrom, keyFrom, metaTo, keyTo = table.unpack(item)
         table.insert(hotkeys, appRemap(metaFrom, keyFrom, metaTo, keyTo))
-
-        logger.d("Mapping " ..
-            tostring(metaFrom) .. tostring(keyFrom) .. " -> " ..
-            tostring(metaTo) .. tostring(keyTo))
+        logMapping(metaFrom, keyFrom, metaTo, keyTo)
     end
-
     return hotkeys
 end
 
-function bind_exclude(appTitles, keymap)
-    appTitles_str = ""
-    for k, v in pairs(appTitles) do
-        appTitles_str = appTitles_str .. k .. ", "
+-- App Watcher Functions
+local keyboardHotkeys
+local kanaEventTap
+local kanaHandlingEnabled = false
+local mouseSideButtonEventTap
+
+local function enableKeys()
+    for _, hk in pairs(keyboardHotkeys) do
+        hk:enable()
     end
+end
 
-    logger.d("Found binding for app " .. appTitles_str)
+local function disableKeys()
+    for _, hk in pairs(keyboardHotkeys) do
+        hk:disable()
+    end
+end
 
-    local eventtap = nil
-    local enabled = false
-    local hotkeys = keymapToHotkeys(keymap)
+function enableKanaAbc()
+    logger.d("enable kana")
 
-    local function enableKeys()
-        for i, hotkey in pairs(hotkeys) do
-            hotkey:enable()
+    if not kanaEventTap then
+        local prevKeyCode
+        local prevFlags
+        local keyMap = hs.keycodes.map
+
+        local function isCmdKeyUp(e)
+            return not e:getFlags()['cmd'] and e:getType() == hs.eventtap.event.types.flagsChanged
         end
-    end
 
-    local function disableKeys()
-        for i, hotkey in pairs(hotkeys) do
-            hotkey:disable()
-        end
-    end
-
-    local function enableKanaAbc()
-        logger.d("enable kana")
-
-        if eventtap == nil then
-            local prevKeyCode
-            local prevFlags
-            local keyMap = hs.keycodes.map
-
-            local function isCmdKeyUp(e)
-                return not(e:getFlags()['cmd']) and e:getType() == hs.eventtap.event.types.flagsChanged
-            end
-
-            local function isCmdKeyAlone(flags)
+        local function isCmdKeyAlone(flags)
+            if flags then
                 return flags['cmd'] and not flags['shift'] and not flags['alt'] and not flags['ctrl']
             end
-
-            local function processKeyUp(e)
-                if prevKeyCode == keyMap['cmd'] then
-                    hs.alert.closeAll()
-                    hs.alert.show('ABC')
-                    hs.eventtap.keyStroke({}, keyMap['eisu'], 0)
-                elseif prevKeyCode == keyMap['rightcmd'] then
-                    hs.alert.closeAll()
-                    hs.alert.show('Kana')
-                    hs.eventtap.keyStroke({}, keyMap['kana'], 0)
-                end
-            end
-
-            eventtap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyDown}, function (e)
-                local keyCode = e:getKeyCode()
-                local flags = e:getFlags()
-
-                if not isCmdKeyUp(e) then
-                    prevKeyCode = keyCode
-                    prevFlags = flags
-                    return
-                end
-
-                if isCmdKeyUp(e) and isCmdKeyAlone(prevFlags) then
-                    processKeyUp(e)
-                end
-
-                prevKeyCode = keyCode
-                prevFlags = flags
-            end)
+            return false
         end
 
-        eventtap:start()
-        enabled = true
-    end
+        local function processKeyUp(e)
+            if prevKeyCode == keyMap['cmd'] then
+                hs.alert.closeAll()
+                hs.alert.show('ABC')
+                hs.eventtap.keyStroke({}, keyMap['eisu'], 0)
+            elseif prevKeyCode == keyMap['rightcmd'] then
+                hs.alert.closeAll()
+                hs.alert.show('Kana')
+                hs.eventtap.keyStroke({}, keyMap['kana'], 0)
+            end
+        end
 
-    local function disableKanaAbc()
-        logger.d("disable kana")
-        eventtap:stop()
-        enabled = false
-    end
+        kanaEventTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyDown}, function (e)
+            local keyCode = e:getKeyCode()
+            local flags = e:getFlags()
 
-    enableKeys()
-    enableKanaAbc()
-
-    windowtap = hs.application.watcher.new(function (name, type, app)
-        if type == hs.application.watcher.activated and has_value(appTitles, name) then
-            items = hs.tabs.tabWindows(app)
-
-            if #(items) > 0 then
-                title = items[1]:title()
+            if not isCmdKeyUp(e) then
+                prevKeyCode = keyCode
+                prevFlags = flags
+                return false
             end
 
-            if enabled and string.find(items[1]:title(), 'DynamoDB共同編集') then
-                hs.alert.closeAll()
-                hs.alert.show("disable key remap")
-                disableKeys()
-                disableKanaAbc()
+            if isCmdKeyUp(e) and isCmdKeyAlone(prevFlags) then
+                processKeyUp(e)
+            end
+
+            prevKeyCode = keyCode
+            prevFlags = flags
+
+            return true
+        end)
+    end
+
+    kanaEventTap:start()
+    kanaHandlingEnabled = true
+end
+
+function disableKanaAbc()
+    logger.d("disable kana")
+    kanaEventTap:stop()
+    kanaHandlingEnabled = false
+end
+
+function appWatcherFn(appTitles)
+    return function(name, type, app)
+        if type == hs.application.watcher.activated and has_value(appTitles, name) then
+            local items = hs.tabs.tabWindows(app)
+
+            if items and #(items) > 0 then
+                local title = items[1]:title()
+
+                if kanaHandlingEnabled and string.find(title, 'DynamoDB共同編集') then
+                    hs.alert.closeAll()
+                    hs.alert.show("disable key remap")
+                    disableKeys()
+                    disableKanaAbc()
+                end
             end
         elseif type == hs.application.watcher.activated then
-            if enabled == false then
+            if not kanaHandlingEnabled then
                 hs.alert.closeAll()
                 hs.alert.show("enable key remap")
                 enableKeys()
                 enableKanaAbc()
             end
         end
-    end)
-
-    windowtap:start()
+    end
 end
 
--- カーソル移動
-bind_exclude({'WorkSpacesClient.macOS', 'Visual Studio', 'Google Chrome'}, {
-    { {'ctrl'}, 'f', {}, 'right' },
-    { {'ctrl'}, 'b', {}, 'left' },
-    { {'ctrl'}, 'n', {}, 'down' },
-    { {'ctrl'}, 'p', {}, 'up' },
-})
+local function createAppWatcher(appTitles, keymap)
+    keyboardHotkeys = keymapToHotkeys(keymap)
+    enableKeys()
+    enableKanaAbc()
 
+    local watcher = hs.application.watcher.new(appWatcherFn(appTitles))
+    watcher:start()
+
+    return watcher
+end
+
+-- Mouse Button Logic
+local function handleSideButtonEvent(e)
+	if e:getButtonState(3) then
+		hs.eventtap.keyStroke({ "cmd" }, "[")
+        hs.alert.show("Goto Previous Page")
+		return true
+	elseif e:getButtonState(4) then
+		hs.eventtap.keyStroke({ "cmd" }, "]")
+        hs.alert.show("Goto Next Page")
+		return true
+	else
+		return false
+	end
+end
+
+local function initializeMouseEventTap()
+    mouseSideButtonEventTap = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, handleSideButtonEvent)
+    mouseSideButtonEventTap:start()
+end
+
+-- Initialization Logic
+local function initialize()
+    createAppWatcher(
+        {'WorkSpacesClient.macOS', 'Visual Studio', 'Google Chrome'},
+        {
+            { {'ctrl'}, 'f', {}, 'right' },
+            { {'ctrl'}, 'b', {}, 'left' },
+            { {'ctrl'}, 'n', {}, 'down' },
+            { {'ctrl'}, 'p', {}, 'up' },
+        }
+    )
+
+    initializeMouseEventTap()
+end
+
+initialize()
